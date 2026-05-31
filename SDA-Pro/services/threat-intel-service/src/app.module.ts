d97e42d64c6d135c8ee162602365c3746a084435
umerrfarooq <umer252752@gmail.com>
2026-05-31 17:50:32 +0500

    feat(B): response orchestration, threat intel controllers + seed data

--- SDA-Pro/services/threat-intel-service/src/app.module.ts
+++ SDA-Pro/services/threat-intel-service/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ThreatIntelController } from './controllers/threat-intel.controller';
+import { ThreatIntelService } from './services/threat-intel.service';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    controllers: [ThreatIntelController],
+    providers: [ThreatIntelService],
 })
 export class AppModule { }
